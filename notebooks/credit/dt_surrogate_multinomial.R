846f734df609fab7e6fae4cac10e993f6864a54a
navdeep-G <mr.navdeepgill@gmail.com>
2018-09-28 11:17:37 -0700

    Modify dt surrogate code and add image for simulated data

--- notebooks/credit/dt_surrogate_multinomial.R
+++ notebooks/credit/dt_surrogate_multinomial.R
@@ -5,53 +5,59 @@ library(caret)
 library(ggplot2)
 library(scales)
 
-#Helper function(s)
-ggplotConfusionMatrix <- function(m){
-  mytitle <- paste("Accuracy", percent_format()(m$overall[1]),
-                   "Kappa", percent_format()(m$overall[2]))
-  p <-
-    ggplot(data = as.data.frame(m$table) ,
-           aes(x = Reference, y = Prediction)) +
-    geom_tile(aes(fill = log(Freq)), colour = "white") +
-    scale_fill_gradient(low = "white", high = "steelblue") +
-    geom_text(aes(x = Reference, y = Prediction, label = Freq)) +
-    theme(legend.position = "none") +
-    ggtitle(mytitle)
-  return(p)
+dt_surrogate <- function(data_path, ignore_cols, target, plot_title, output_file_name) {
+  
+  #Helper function(s)
+  ggplot_confusion_matrix <- function(m){
+    mytitle <- paste("Accuracy", percent_format()(m$overall[1]),
+                     "Kappa", percent_format()(m$overall[2]))
+    p <-
+      ggplot(data = as.data.frame(m$table) ,
+             aes(x = Reference, y = Prediction)) +
+      geom_tile(aes(fill = log(Freq)), colour = "white") +
+      scale_fill_gradient(low = "white", high = "steelblue") +
+      geom_text(aes(x = Reference, y = Prediction, label = Freq)) +
+      theme(legend.position = "none") +
+      ggtitle(mytitle)
+    return(p)
+  }
+  
+  #Setup dataset for rpart
+  data = read.csv(data_path)
+  data_train = data[!colnames(data) %in% ignore_cols]
+  
+  #Construct rpart tree
+  tree_data <- rpart(as.formula(paste0(target, "~ .")), data = data_train, method = "class")
+  
+  #Plot rpart tree
+  rpart.plot(tree_data, main = plot_title)
+  
+  #Check for important variables
+  tree_data$variable.importance
+  
+  #Predict
+  predict <- as.data.frame(predict(tree_data, data_train))
+  predicted_class <- as.data.frame(colnames(predict)[max.col(predict,ties.method="first")])
+  colnames(predicted_class) <- c("pred_rpart")
+  
+  #Check performance
+  print("RPart Performance Metrics:")
+  printcp(tree_data) # display the results 
+  plotcp(tree_data) # visualize cross-validation results 
+  summary(tree_data) # detailed summary of splits
+  
+  print("RPart Confusion Matrix")
+  cfm <- confusionMatrix(predicted_class$pred_rpart, data_train[,target])
+  cfm
+  
+  #ggplot version of confusion matrix
+  ggplot_confusion_matrix(cfm)
+  
+  #Export tree image
+  png(paste0("./images/", output_file_name), width = 800, height = 600)
+  rpart.plot(tree_data, main = plot_title)
+  dev.off()
+  
 }
 
-#Setup dataset for rpart
-credit = read.csv("./output/credit_mult_preds.csv")
-credit_train = credit[!colnames(credit) %in% c("prob_PAY_0_class0","prob_PAY_0_class1","prob_PAY_0_class2","prob_PAY_0_class3",
-                             "prob_PAY_0_class4","prob_PAY_0_class5","prob_PAY_0_class6","prob_PAY_0_class7",
-                             "prob_PAY_0_class8", "PAY_0")]
-
-#Construct rpart tree
-tree_credit <- rpart(pred_PAY_0 ~ ., data = credit_train, method = "class")
-
-#Plot rpart tree
-rpart.plot(tree_credit, main = "Credit Decision Tree Surrogate Model")
-
-#Check for important variables
-tree_credit$variable.importance
-
-#Predict
-predict <- as.data.frame(predict(tree_credit, credit_train))
-predicted_class <- as.data.frame(colnames(predict)[max.col(predict,ties.method="first")])
-colnames(predicted_class) <- c("pred_PAY_0_rpart")
-
-#Check performance
-printcp(tree_credit) # display the results 
-plotcp(tree_credit) # visualize cross-validation results 
-summary(tree_credit) # detailed summary of splits
-
-cfm <- confusionMatrix(predicted_class$pred_PAY_0_rpart, credit_train$pred_PAY_0)
-cfm
-
-#ggplot version of confusion matrix
-ggplotConfusionMatrix(cfm)
-
-#Export tree image
-png("./images/dt_surrogate_credit.png", width = 800, height = 600)
-rpart.plot(tree_credit, main = "Credit Decision Tree Surrogate Model")
-dev.off()
+#dt_surrogate("./output/simulated_mult_preds.csv", ignore_cols, "pred_target", "Simulated Data Decision Tree Surrogate", "dt_surrogate_simulated.png")
